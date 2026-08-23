46487c85ed38a5fe1f93bcf1a9f58d675183d30f
Vedant Vallal <157898727+VedantVallal@users.noreply.github.com>
2026-08-23 22:48:18 +0530

    Time: 0 ms (100%), Space: 15 MB (12.23%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,39 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int t) {
-        vector<int>ans;
-        int i=0,j=nums.size()-1;
-        int sum=0;
-        vector<int>org;
-        org=nums;
-        sort(nums.begin(),nums.end());
-       
-        while(i<j){
-            sum=nums[i]+nums[j];
-            if(sum>t)j--;
-            else if(sum<t)i++;
-            else{
-                ans.push_back(nums[i]);
-                ans.push_back(nums[j]);
-                break;
+        unordered_map<int,int>freq;
+        int need=0;
+        for(int i=0;i<nums.size();i++){
+            need=t-nums[i];
+            if(freq.count(need)){
+                return {i,freq[need]};
             }
+            freq[nums[i]]=i;
         }
-        auto it1 = find(org.begin(),org.end(),ans[0]);
-        int ind1=it1-org.begin();
-        
-        auto it2 = find(org.begin(),org.end(),ans[1]);
-       int ind2=it2-org.begin();
-        
-        if(ind1==ind2){
-            auto it3=find(it2+1,org.end(),ans[1]);
-            int ind3=it3-org.begin();
-            
-        ans[0]=ind1;
-        ans[1]=ind3;
-        return ans;
-        }
-        ans[0]=ind1;
-        ans[1]=ind2;
-        return ans;
+        return {0,0};
     }
 };
