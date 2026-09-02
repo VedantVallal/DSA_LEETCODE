b2b64db255ba4b828ea1a22ce6b54dc8657cca79
Vedant Vallal <157898727+VedantVallal@users.noreply.github.com>
2026-09-02 21:38:16 +0530

    Time: 0 ms (100%), Space: 15.6 MB (98.85%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -5,11 +5,12 @@ public:
         int i=n-2;
         int j=n-1;
 
-        while(i>=0  && nums[i]>=nums[i+1]) i--;
+        while(i>=0 && nums[i]>=nums[i+1])i--;
         if(i>=0){
-        while(nums[j]<nums[i])j--;
-        swap(nums[i],nums[j]);
+            while(nums[j]<=nums[i])j--;
+            swap(nums[i],nums[j]);
+
         }
         reverse(nums.begin()+i+1,nums.end());
-    }
+      }
 };
