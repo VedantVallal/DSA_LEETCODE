9b8bc469be5b79352896dd4e4e7644e7dd485698
Vedant Vallal <157898727+VedantVallal@users.noreply.github.com>
2026-09-04 23:14:03 +0530

    Time: 0 ms (100%), Space: 37.3 MB (10.61%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -5,13 +5,12 @@ public:
         int n=nums.size()-1;
         vector<int>ps;
         int sum=0;
-        if(nums.size()==1)return 0;
         for(auto val:nums){
             sum+=val;
             ps.push_back(sum);
         }
-        for(int i=0;i<nums.size()-1;i++){
             if(ps[0]==ps[n])return 0;
+        for(int i=0;i<nums.size()-1;i++){
             if((ps[n]-ps[i])==ps[i+1])return i+1;
         }
         return -1;
